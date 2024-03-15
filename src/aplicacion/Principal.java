4f4d7df38f85a60232c4df65a968fe9706fd0190
CARLOTA RUIZ CAMPESINO <carlota.ruizcampesino@usp.ceu.es>
2024-03-15 08:59:20 +0100

    Programa acabado

---
+++ src/aplicacion/Principal.java
@@ -0,0 +1,17 @@
+package aplicacion;
+
+import mates.Matematicas;
+
+/**
+ * Clase principal que contiene el método main para ejecutar la aplicación.
+ */
+public class Principal {
+    /**
+     * Método principal que imprime el valor de PI calculado de forma recursiva.
+     * 
+     * @param args los argumentos de la línea de comandos
+     */
+    public static void main(String[] args) {
+        System.out.println("El número PI es " + Matematicas.piLambda(10000));
+    }
+}
